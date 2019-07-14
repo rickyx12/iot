2cafa175adfac886ff023a837c2e1ccd90558969
rickyx12 <aizekrysler@gmail.com>
2019-07-15 00:40:27 +0800

    remove unnecessary code in dashboard controller

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,22 +6,13 @@ class Dashboard extends CI_Controller {
  	public function __construct() {
  		parent::__construct();
  		$this->load->helper('url');
- 		$this->load->model('orders_model');
  	}
 
 	public function index()
 	{
 
-		$totalOrders = $this->orders_model->getAllOrderRequest(date("Y-m-d"))->num_rows();
-		$pendingOrders = $this->orders_model->getOrderRequest(date("Y-m-d"))->num_rows();
-		$deliveredOrders = $this->orders_model->getDeliveredOrderRequest(date("Y-m-d"))->num_rows();
-
-
  		$data = array(
  			"page" => "dashboard-nav", 
- 			'totalOrders' => $totalOrders,
- 			'pendingOrders' => $pendingOrders,
- 			'deliveredOrders' => $deliveredOrders
  		);
 
 		$this->load->view('includes/header',$data);
