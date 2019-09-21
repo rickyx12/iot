9f6979e3f35a5e8147273a7e37ee93a546077f7a
Prncssbby <guico.princess@gmail.com>
2019-09-21 08:14:31 +0800

    added table on dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -9,13 +9,43 @@
         </li>
       </ol>
       <div class="row">
-        <div class="col-md-12">
-          <!-- <h1>Dashboard Page</h1> -->
-        </div>
-      </div>
-      <div class="row">
         <div class="col-md-12" height="80" width="100">
-          <canvas id="temperatureChart" class="w-100 h-100"></canvas>
+
+
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#chart" role="tab" aria-controls="home" aria-selected="true">Chart</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#table" role="tab" aria-controls="profile" aria-selected="false">Table</a>
+            </li>
+          </ul>
+
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="chart" role="tabpanel" aria-labelledby="home-tab">
+              <div class="col-md-12" height="80" width="100">
+                <canvas id="temperatureChart" class="w-100 h-100"></canvas>
+              </div>              
+            </div>
+            <div class="tab-pane fade" id="table" role="tabpanel" aria-labelledby="profile-tab">
+              <div class="table-responsive mt-3">
+                <table id="dashboardTable" class="table table-bordered table-striped w-100">
+                  <thead>
+                    <tr>
+                      <th>Temp</th>
+                      <th>Humid</th>
+                      <th>Soil Moist</th>
+                      <th>pH</th>
+                      <th></th>
+                      <th></th>
+                      <th></th>
+                      <th>Details</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>              
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- assets/js/dashboard/dashboard.js
+++ assets/js/dashboard/dashboard.js
@@ -7,6 +7,10 @@ $(function(){
 	  var tempArr = [];
 	  var humidArr = [];
 	  var soilMoistArr = [];
+	  var phArr = [];
+	  var soilTypeArr = [];
+	  var soilTypeArr1 = [];
+	  var soilTypeArr2 = [];
 
 	  $.ajax({
 	  	url:base_url+'Parameters/getLatestParametersJSON',
@@ -26,6 +30,10 @@ $(function(){
 	  			tempArr.push(value.temperature);
 	  			humidArr.push(value.humidity);
 	  			soilMoistArr.push(value.soil_moisture);
+	  			phArr.push(value.ph);
+	  			soilTypeArr.push(value.soil_type);
+	  			soilTypeArr1.push(value.soil_type1);
+	  			soilTypeArr2.push(value.soil_type2);
 	  		});
 
 
@@ -59,7 +67,16 @@ $(function(){
 		        fill: false,
 		        lineTension: 0,
 		        radius: 5
-		      }		      
+		      },
+		      {
+		        label: "pH",
+		        data: phArr,
+		        backgroundColor: "yellow",
+		        borderColor: "yellow",
+		        fill: false,
+		        lineTension: 0,
+		        radius: 5
+		      }			      		      
 		    ]
 		  };
 
@@ -93,5 +110,69 @@ $(function(){
 	  	}
 	  });
 	
+
+	 var dashboardTable = $('#dashboardTable').DataTable({
+	 					processing:true,
+						serverSide:true,								
+						ajax:{
+							url: base_url+'Parameters/datatableJSON',
+						},
+						columns:[
+							{ 
+								data:null,
+								render:function(data,type,row) {
+									return data.temp
+								}
+							},
+							{ 
+								data:null,
+								render:function(data,type,row) {
+									return data.humidity
+								}
+							},
+							{ 
+								data:null,
+								render:function(data,type,row) {
+									return data.soil_moisture
+								}
+							},
+							{ 
+								data:null,
+								render:function(data,type,row) {
+									return data.ph
+								}
+							},							
+							{ 
+								data:null,
+								render:function(data,type,row) {
+									return data.soil_type
+								}
+							},	
+							{ 
+								data:null,
+								render:function(data,type,row) {
+									return data.soil_type1
+								}
+							},		
+							{ 
+								data:null,
+								render:function(data,type,row) {
+									return data.soil_type2
+								}
+							},																																		
+							{
+								data:null,
+								render:function(data,type,row) {
+
+									var splitStr = data.details.split(" ");
+									var date = splitStr[0];
+									var time = splitStr[1];
+
+									return formatDate(date)+" @ "+formatTime(time);
+								}
+							}
+						]
+					});
+
 
 });
